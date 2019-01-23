eb8a8328057820d9ac667819b3855da2510518c8
lacey <hiu@example.com>
2019-01-23 15:42:51 +0000

    add checkbox

--- client/src/views/bucketlist_view.js
+++ client/src/views/bucketlist_view.js
@@ -9,21 +9,38 @@ BucketlistView.prototype.render = function (enteredWish) {
   const wishesContainer = document.createElement('div');
   wishesContainer.id = 'newwishes';
 
-  const newWish = this.createDetail(enteredWish.wish);
-  wishesContainer.appendChild(newWish);
+  // const newWish = this.createDetail(enteredWish.wish);
+  // wishesContainer.appendChild(newWish);
 
   const deleteButton = this.createDeleteButton(enteredWish._id);
   wishesContainer.appendChild(deleteButton);
 
+
+  const checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.value = "value";
+  checkbox.id = "id";
+  wishesContainer.appendChild(checkbox);
+
+  const label = document.createElement('label')
+  label.htmlFor = "id";
+  label.textContent = (enteredWish.wish)
+
+  wishesContainer.appendChild(label);
+
+
   this.container.appendChild(wishesContainer);
+
+
 };
 
 
-BucketlistView.prototype.createDetail = function (text) {
-  const detail = document.createElement('p');
-  detail.textContent = text;
-  return detail;
-};
+// BucketlistView.prototype.createDetail = function (text) {
+//   const detail = document.createElement('p');
+//   detail.textContent = text;
+//   return detail;
+// };
 
 BucketlistView.prototype.createDeleteButton = function (wishId) {
   const button = document.createElement('button');
