76821690805908b0315b98ef6b20185399c33d20
lacey <hiu@example.com>
2019-01-23 11:20:42 +0000

    add initial file structure and show js in console browser

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
